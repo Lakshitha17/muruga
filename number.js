a07d417603fda97828234e68675d607e95d42915
lakshitha <lakshithaneelakandan@gmail.com>
2026-09-12 10:30:28 +0530

    Add number problem

---
+++ number.js
@@ -0,0 +1,127 @@
+// 1.sum of two numbers
+// let num1 = 10
+// let num2 = 20
+// console.log(num1 + num2)
+
+
+//2.mul of two numbers
+// let num1 = 20;
+// let num2 = 30;
+// console.log(num1*num2);
+
+//3.greatest of three numbers
+// let num;
+// let a,b,c
+// if(a>b)
+// {
+//     console.log("a is big")
+// }
+// else if(b>c)
+// {
+//     console.log("b is big")
+// }
+// else{
+//     console.log("c is big")
+// }
+
+//4.vowel and consonant
+// let ch = 'a';
+// if((ch=='k')||(ch=='a')||(ch=='j'))
+// {
+//     console.log("vowel")
+// }
+// else{
+//     console.log("not a vowel");
+// }
+
+//5.Even number
+// let num1 = 20;
+// if(num1%2==0)
+// {
+//     console.log("Even number")
+// }
+// else{
+//     console.log("not even number")
+// }
+
+// 6. positive or negative 
+// let num = 12;
+//     if(num>0)
+//     {
+//         console.log("Positive number")
+//     }
+//     else{
+//         console.log("negative number")
+//     }
+
+// 7. prime or not
+// let num = 12;
+// let count = 0;
+
+// for(let i = 1; i <= num; i++)
+// {
+//     if(num % i == 0)
+//     {
+//         count++;
+//     }
+// }
+
+// if(count == 2)
+// {
+//     console.log("Prime number");
+// }
+// else
+// {
+//     console.log("Not a prime number");
+// }
+
+//8.palindrome
+// let num = 121;
+// let rev,rem=0,temp
+// temp=num;
+// while(num>0)
+// {
+//     rem = num%10;
+//     rev = (rev*10)+rem;
+//     num = parseInt(num/10);
+// }
+// if(temp==rev)
+// {
+//     console.log("palindrome")
+// }
+// else
+// {
+//     console.log("not a palindrome")
+// }
+
+// 9.Armstrong
+// let num = 153;
+// let sum=0,rem,temp;
+// temp=num;
+// while(num>0)
+// {
+//     rem = num%10;
+//     sum = sum+rem*rem*rem;
+//     num = parseInt(num/10)
+// }
+// if(temp==sum)
+// {
+//     console.log("Armstromg number")
+// }
+// else
+// {
+//     console.log("not Armstrong number")
+// }
+
+//10.sum of digits
+// let n = 123;
+// let sum = 0;
+// while(n>0)
+// {
+//     let digit = n%10;
+//     sum = sum+digit;
+//     n = parseInt(n/10);
+// }
+// console.log(sum)
+
+//11.
